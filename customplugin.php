ce1c3b5ce648f5ccce6cef23ffd2826571e12345
= <nguyenvantrong@live.com>
2022-09-27 13:58:37 +0700

    inint

---
+++ customplugin.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * Plugin Name: Custom Plugin For Test
+ * Author: Chuan Team
+ * URI: chuanw3s.com
+ * Version: 1.0.1
+ * Description: Custom Test
+ * License:           GNU General Public License v2
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+ * GitHub Plugin URI: https://github.com/afragen/github-updater
+ * GitHub Branch:     master
+ * 
+ */
+?>
